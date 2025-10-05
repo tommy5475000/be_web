0ca348558fd5df6196c9c151c9a6199f6326aac0
Hà Đạt Cường <133970445+tommy5475000@users.noreply.github.com>
2025-10-05 15:32:58 +0700

    05-10-2025

--- src/ApiKiot/categories.mjs
+++ src/ApiKiot/categories.mjs
@@ -1,1 +1,21 @@
-export const getAllCategories
+import getAccessToken from './auth.mjs';
+
+export const getAllCategories = async (accessToken) => {
+    if (!accessToken) {
+        return null
+    }
+    
+
+};
+const saveCategoriesToDatabase = () => {};
+const upCategories = async () => {
+  const accessToken = await getAccessToken();
+  if (accessToken) {
+    const category = await getAllCategories(accessToken);
+    if (category) {
+      await saveCategoriesToDatabase(category);
+    }
+  }
+};
+
+export { saveCategoriesToDatabase, upCategories };
