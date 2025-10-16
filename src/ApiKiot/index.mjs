33ca2327e0ef6c2fee2ff80a58aa411e5e90a2ae
tommy547 <datcuongha@gmail.com>
2025-10-16 08:54:48 +0700

    16-10-25

--- src/ApiKiot/index.mjs
+++ src/ApiKiot/index.mjs
@@ -9,59 +9,59 @@ import { upCategories } from "./categories.mjs";
 import { updateProducts } from "./productKiot.mjs";
 
 // Gán ngày theo giờ VN
-// let today = new Date().toLocaleDateString("en-CA", {
-//   timeZone: "Asia/Ho_Chi_Minh",
-// });
-// console.log(`🚀 App started - Today is: ${today}`);
+let today = new Date().toLocaleDateString("en-CA", {
+  timeZone: "Asia/Ho_Chi_Minh",
+});
+console.log(`🚀 App started - Today is: ${today}`);
 
-// // Cron từ 8h đến 23h mỗi phút
-// cron.schedule(
-//   "*/5 8-23 * * *",
-//   () => {
-//     console.log("✅ [Cron] Running updateBills...");
-//     updateBills(today)
-//       .then(() => {
-//         console.log(`✅ Bills updated: ${today}`);
-//       })
-//       .catch((err) => {
-//         console.error("❌ updateBills error:", err);
-//       });
-//   },
-//   {
-//     timezone: "Asia/Ho_Chi_Minh"
-//   }
-// );
+// Cron từ 8h đến 23h mỗi phút
+cron.schedule(
+  "*/5 8-23 * * *",
+  () => {
+    console.log("✅ [Cron] Running updateBills...");
+    updateBills(today)
+      .then(() => {
+        console.log(`✅ Bills updated: ${today}`);
+      })
+      .catch((err) => {
+        console.error("❌ updateBills error:", err);
+      });
+  },
+  {
+    timezone: "Asia/Ho_Chi_Minh"
+  }
+);
 
-// cron.schedule(
-//   "0 0 * * *",
-//   () => {
-//     today = new Date().toLocaleDateString("en-CA", {
-//       timeZone: "Asia/Ho_Chi_Minh",
-//     });
-//     console.log(`📅 Chuyển sang ngày mới: ${today}`);
-//   },
-//   {
-//     timezone: "Asia/Ho_Chi_Minh"
-//   }
-// );
+cron.schedule(
+  "0 0 * * *",
+  () => {
+    today = new Date().toLocaleDateString("en-CA", {
+      timeZone: "Asia/Ho_Chi_Minh",
+    });
+    console.log(`📅 Chuyển sang ngày mới: ${today}`);
+  },
+  {
+    timezone: "Asia/Ho_Chi_Minh"
+  }
+);
 
-// // // Giữ tiến trình sống
-// setInterval(() => {}, 1000 * 60 * 60);
-//   updateBills(today).then(() => console.log("Get bill successfully."));
+// // Giữ tiến trình sống
+setInterval(() => {}, 1000 * 60 * 60);
+  updateBills(today).then(() => console.log("Get bill successfully."));
   
   
-// // // Thiết lập cron job nếu cần
-// cron.schedule("*/1 8-23 * * *", () => {
-//   // Gói toàn bộ logic vào một hàm async
-//   (async () => {
-//     try {
-//       await updateBills(today);
-//       console.log("✅ Bills updated successfully.");
-//     } catch (err) {
-//       console.error("❌ Error while updating bills:", err);
-//     }
-//   })(); // <-- gọi hàm async tự chạy
-// });
+// // Thiết lập cron job nếu cần
+cron.schedule("*/1 8-23 * * *", () => {
+  // Gói toàn bộ logic vào một hàm async
+  (async () => {
+    try {
+      await updateBills(today);
+      console.log("✅ Bills updated successfully.");
+    } catch (err) {
+      console.error("❌ Error while updating bills:", err);
+    }
+  })(); // <-- gọi hàm async tự chạy
+});
 
 // today.setDate(today.getDate() - 1);
 // updateReturnInvoice().then(()=> console.log("Api bill trả hàng thành công"))
@@ -70,7 +70,7 @@ import { updateProducts } from "./productKiot.mjs";
 // updateSups().then(()=> console.log("Nhà cung cấp đã api thành công "))
 // updateProducts().then(()=> console.log("Danh mục hành hóa đã api thành công "))
 // updateOneBill({code:"HD362592"}).then(()=>console.log("Đã get bill"));
-updateBills("2025-10-14").then(() => console.log("Bills updated successfully."));
+// updateBills("2025-10-15").then(() => console.log("Bills updated successfully."));
 // updateTransfers().then(()=>console.log("Đã lấy hết phiếu điều chuyển."));
 // updateBranches().then(()=>console.log("Đã lấy hết chi nhánh"))
 // updatePur().then(()=>console.log("Đã cập nhập phiếu nhập hàng"))
